02d10eb5d58161a9979047fb1505ffac53f167c5
zashii <hideki.ishizashi@gmail.com>
2021-07-17 15:28:42 +0900

    69th

--- 4.pythonSutdy.py
+++ 4.pythonSutdy.py
@@ -1,3 +1,8 @@
-C = "hoge"
-print(c)
-if 10 > 1
+smash = 'スマッシュ'
+stroke = 'ストローク'
+for count in range(10):
+    if count % 2 == 0:
+        print (smash)
+    else:
+        print(stroke)
+            
