c0fb7b74591ddd2315c17c87bc423f7a9523ecb8
zashii <hideki.ishizashi@gmail.com>
2021-07-18 16:42:37 +0900

    70th

--- 4.pythonSutdy.py
+++ 4.pythonSutdy.py
@@ -1,8 +1,17 @@
-smash = 'スマッシュ'
-stroke = 'ストローク'
-for count in range(10):
-    if count % 2 == 0:
-        print (smash)
-    else:
-        print(stroke)
-            
+class Micheal:
+    count = 0
+    max = 5
+
+    def teach(self):
+        if self.__class__.count < self.__class__.max:
+            print("もっと強く")
+        else:
+            print("OK") 
+        self.__class__.count += 1
+
+
+onil = Micheal()
+for i in range(4):
+    print("スマッシュ")
+    onil.teach()
+
