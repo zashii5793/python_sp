72873a653c6450804efc0b6671f27be8a0276187
zashii <hideki.ishizashi@gmail.com>
2021-07-19 06:57:58 +0900

    71th

--- 4.pythonSutdy.py
+++ 4.pythonSutdy.py
@@ -1,17 +1,6 @@
-class Micheal:
-    count = 0
-    max = 5
-
-    def teach(self):
-        if self.__class__.count < self.__class__.max:
-            print("もっと強く")
-        else:
-            print("OK") 
-        self.__class__.count += 1
-
-
-onil = Micheal()
-for i in range(4):
-    print("スマッシュ")
-    onil.teach()
-
+print('身長(ｃｍ)は?')
+height = 175
+bmi = 22
+weight = bmi * (height / 100) ** 2
+print('身長が' + str(height) + 'cmの場合の標準体重は', end= '')
+print('{:.2f}kgです。'.format(weight))
