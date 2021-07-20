3f954976eed9a2cd6c64d2a1ee1f5ddc249c5922
zashii <hideki.ishizashi@gmail.com>
2021-07-20 21:06:13 +0900

    72th

--- 4.pythonSutdy.py
+++ 4.pythonSutdy.py
@@ -1,6 +1,8 @@
-print('身長(ｃｍ)は?')
-height = 175
-bmi = 22
-weight = bmi * (height / 100) ** 2
-print('身長が' + str(height) + 'cmの場合の標準体重は', end= '')
-print('{:.2f}kgです。'.format(weight))
+from typing import Counter
+
+
+counter = 0
+while counter < 10:
+    print('スマッシュ')
+    counter = counter + 1
+    
