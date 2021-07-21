2447b37766be2a619d61c1bfa0786393705b8373
zashii <hideki.ishizashi@gmail.com>
2021-07-22 05:54:09 +0900

    74th

--- 4.pythonSutdy.py
+++ 4.pythonSutdy.py
@@ -2,3 +2,14 @@ import random
 attacks = ['ストローク', 'ボレー', 'スマッシュ', 'ロブ']
 for count in range(5):
     print(random.choice(attacks))
+
+sound = {
+    'A' : 'a',
+    'B' : 'b',
+    'C' : 'c',
+    'D' : 'd',
+    'E' : 'e',
+    'F' : 'f',
+}
+for key, value in sound.items():
+    print(key, value)
