3a791ac667b2488dfa7418b46b0b1c59cdee41c8
zashii <hideki.ishizashi@gmail.com>
2021-07-27 08:34:23 +0900

    79th

--- prototype.py
+++ prototype.py
@@ -3,14 +3,14 @@ class Ptna:
       self.name = name
       self.responder = Responder('What')
 
-def dialogue(self, input):
-    return self.responder.response(input)
+    def dialogue(self, input):
+        return self.responder.response(input)
 
-def get_responder_name(self):
-    return self.responder.name
+    def get_responder_name(self):
+        return self.responder.name
 
-def get_name(self):
-    return self.name
+    def get_name(self):
+        return self.name
 
 class Responder:
     def __init__(self, name):
@@ -19,17 +19,15 @@ class Responder:
     def response(self, input):
         return '{}ってなに？'.format(input)
 
-def prompt(obj):
-    return obj.get_name() + ":" + obj.get_responder_name() + '> '
+    def prompt(obj):
+        return obj.get_name() + ":" + obj.get_responder_name() + '> '
     
-
-
-print('Ptna System prototype : ptna')
-ptna = Ptna('ptna')
+    print('Ptna System prototype : ptna')
+    ptna = Ptna('ptna')
 
 while True:
     inputs = input(' > ')
-    if not inputs
+    if not inputs:
       print('バイバイ')
       break
     response = ptna.dialogue(inputs)
