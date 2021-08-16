752d7a3392063d25c42e28452e3dc15f4a3f1bcb
zashii5793 <hideki.ishizashi@gmail.com>
2021-08-17 06:58:55 +0900

    100th

--- ptna.py
+++ ptna.py
@@ -27,3 +27,28 @@ class Ptna:
         return self.name
         
             
+class Emotion:
+    MOOD_MIN = -15
+    MOOD_MAX = 15
+    MOOD_RECOVERY = 0.5
+
+    def __init__(self, dictionary) :
+        self.dictionary = dictionary
+        self.mood = 0
+
+    def update(self, input):
+        for ptn_item in self.dictionary.pattern:
+            if ptn_item.match(input):
+                self.adjust_mood(ptn_item.modify)
+                break    
+            if self.mood < 0:
+                self.mood += Emotion.MOOD_RECOVERY
+            elif self.mood > 0:
+                self.mood = Emotion.MOOD_RECOVERY
+                
+    def adjust_mood(self, val):
+        self.mood += int(val)
+        if self.mood > Emotion.MOOD_MAX:
+            self.mood = Emotion.MOOD_MAX
+        elif self.mood < Emotion.MOOD_MIN:
+            self.mood = Emotion.MOOD_MIN                        
