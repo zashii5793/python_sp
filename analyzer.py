10bee44c5f2275ba07922d3591457afad3d3632b
zashii5793 <hideki.ishizashi@gmail.com>
2021-09-18 07:42:02 +0900

    131th

--- analyzer.py
+++ analyzer.py
@@ -1,4 +1,16 @@
 from janome.tokenizer import Tokenizer
-t = Tokenizer()
-tokens = t.tokenize('わたしはPythonのプログラムです')
-print(tokens[0].surface)
+import re
+
+def analyze(text):
+    t = Tokenizer()
+    tokens = t.tokenize(text)
+    result = []
+
+    for token in tokens:
+        result.append(
+            [token.surface,
+            token.part_of_speech] )
+    return(result) 
+
+def keyword_check(part):
+    return re.match('名刺,(一般|固有名詞|サ変接続|形容動詞語幹)', part)
