359baf34df7d2f320b8de2dba873ed12302a46e3
zashii5793 <hideki.ishizashi@gmail.com>
2021-09-23 05:09:41 +0900

    136th

--- sample.py
+++ sample.py
@@ -5,3 +5,15 @@ dictionary = {
     'D': 'え',
 }
 print(dictionary.get('D'))
+
+
+lasts = ['A','B']
+firsts = ['C','D']
+for last, first in zip(lasts, firsts):
+    print(last+first)   
+
+
+lasts = ['A','B']
+
+for i, last in enumerate(lasts) :
+  print(f'{i}番目の{last}です')
