1dafe3dec800182a45513e4459ad50c0e66e096a
zashii5793 <hideki.ishizashi@gmail.com>
2021-10-04 20:53:39 +0900

    146th

---
+++ excel_python.py
@@ -0,0 +1,13 @@
+import pandas as pd
+from glob import glob
+
+filepaths = glob('source/*.xlsx')
+filepaths
+filepath = filepaths[0]
+filepath
+
+_df = pd.read_excel(filepath)
+_df
+
+columns = _df.iloc[10, [1, 2, 4, 10, 11, 14]]
+columns
