ef6acad6b9e0a43248c7e705115dad5ffb9a6cbf
zashii5793 <hideki.ishizashi@gmail.com>
2021-10-06 07:32:42 +0900

    148th

--- excel_python.py
+++ excel_python.py
@@ -13,3 +13,12 @@ columns = _df.iloc[10, [1, 2, 4, 10, 11, 14]]
 columns
 
 _df.iloc[11:23, [1, 2, 4, 10, 11, 14]]
+
+def extract(filepath):
+    _df = pd.read_excel(filepath)
+    columns = _df.iloc[10, [1, 2, 4, 10, 11, 14]]
+    df = _df.iloc[11:23, [1, 2, 4, 10, 11, 14]]
+    df.columns = columns
+    df['企業コード']　 = df.iloc[3, 4]
+    return df
+    
