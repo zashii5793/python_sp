9beaef14bd9bab50c832900d66259443305a2ced
zashii5793 <hideki.ishizashi@gmail.com>
2021-10-18 07:52:55 +0900

    158th

--- main.py
+++ main.py
@@ -8,8 +8,23 @@ st.write('DataFrame')
 
 df = pd.DataFrame({
     '1列目': [1, 2, 3, 4],
-    '2列目': [1, 2, 3, 4]
+    '2列目': [10, 20, 30, 40]
 })
 
+#動的な表を作成したい場合
+st.dataframe(df.style.highlight_max(axis=0), width=100, height=100)
 
-st.write(df)
+#static(静的)な表を作成したい場合
+st.table(df.style.highlight_max(axis=0)
+
+"""
+# 章
+## 節
+### 項
+
+'''python
+import streamlit as st
+import numpy as np
+import pandas as pd
+'''
+"""
