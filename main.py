960c80cafa15e8c5d4886efc11827270e6e1d09d
zashii5793 <hideki.ishizashi@gmail.com>
2021-10-22 21:27:02 +0900

    161th

--- main.py
+++ main.py
@@ -18,14 +18,3 @@ st.dataframe(df.style.highlight_max(axis=0), width=100, height=100)
 #static(静的)な表を作成したい場合
 st.table(df.style.highlight_max(axis=0)
 
-"""
-# 章
-## 節
-### 項
-
-'''python
-import streamlit as st
-import numpy as np
-import pandas as pd
-'''
-"""
